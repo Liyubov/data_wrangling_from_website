2512c2aa0d0763beb6ef8df224302e2ce73fd692
Liubov <lyubov78@gmail.com>
2020-03-18 19:03:40 +0100

    Add files via upload

--- python_code/wrangling_data_gas_website.py
+++ python_code/wrangling_data_gas_website.py
@@ -12,34 +12,39 @@ import pandas as pd
 from datetime import datetime
 
 
-# monthly price
-dls = "https://www.eia.gov/dnav/ng/hist_xls/RNGWHHDm.xls"
-resp = requests.get(dls)
-
-output = open('monthlyprice.xls', 'wb')
-output.write(resp.content)
-output.close()
 
 
+def monthlyprice():
+    
+    # monthly price
+    dls = "https://www.eia.gov/dnav/ng/hist_xls/RNGWHHDm.xls"
+    resp = requests.get(dls)
+    
+    output = open('monthlyprice.xls', 'wb')
+    output.write(resp.content)
+    output.close()
+    
+    
+    
+    data = pd.read_excel (r'monthlyprice.xls',sheet_name='Data 1')
+    data.head()
+    
+    
+    # delet first rows of data 
+    data = data.iloc[2:]
+    
+    print(data.columns)
+    #rename columns 
+    data.columns = ['Date', 'Price']
+    
+    # make column Date in datetime format
+    data['Date'] = pd.to_datetime(data['Date'])
+    data.head()
+    
+    data.to_csv(r'monthlyprice_1.csv')#, index = None, header=True)    
+    
+    return data
 
-data = pd.read_excel (r'monthlyprice.xls',sheet_name='Data 1')
-data.head()
 
-
-# delet first rows of data 
-data = data.iloc[2:]
-
-print(data.columns)
-#rename columns 
-data.columns = ['Date', 'Price']
-
-# make column Date in datetime format
-data['Date'] = pd.to_datetime(data['Date'])
-
-data.head()
-
-data.to_csv(r'monthlyprice_1.csv')#, index = None, header=True)
-
-
-#if __name__ == '__main__':
-#    monthly_price() # execute function
+if __name__ == '__main__':
+    monthlyprice() # execute function
